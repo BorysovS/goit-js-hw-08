b65f362b4f44c9f9edd9b689bfabbfcc88996f55
BorysovS <s.borisovbar@gmail.com>
2022-12-18 15:43:36 +0200

    3rd Task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 const throttle = require('lodash.throttle');
+
+const feedBackForm = document.querySelector('.feedback-form')
+const formInput = document.querySelector('.feedback-form input')
+const textAreaForm = document.querySelector('.feedback-form textarea')
+
+const formUserData = {};
+const STORAGE_KEY = "feedback-form-state"
+
+feedBackForm.addEventListener('submit', onSubmit);
+feedBackForm.addEventListener('input', onInputUserData);
+
+onGetDataUserInput()
+
+
+function onSubmit(evt) { 
+    evt.preventDefault();
+    feedBackForm.reset();
+    console.log(formUserData)
+    localStorage.removeItem('STORAGE_KEY');
+
+}
+
+function onInputUserData(evt) {
+    formUserData[evt.target.name] = evt.target.value;
+    const formUserDataJSON = JSON.stringify(formUserData);
+    localStorage.setItem('STORAGE_KEY', formUserDataJSON)
+};
+
+function onGetDataUserInput() {
+    
+    const saveDataUser = JSON.parse(localStorage.getItem('STORAGE_KEY'));
+     
+    if (saveDataUser) { 
+        formInput.value = saveDataUser.email ?? '';
+        textAreaForm.value = saveDataUser.message ?? '';
+    }
+        
+}
